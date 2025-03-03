bce6af8d18c498be05222c17d24ca2f378068dc9
marceloprates <marceloorp@gmail.com>
2025-03-03 14:59:03 -0300

    updated app

--- prettymaps/app.py
+++ prettymaps/app.py
@@ -2,13 +2,13 @@ import streamlit as st
 import logging
 from matplotlib import pyplot as plt
 import subprocess
-from draw import plot, presets
+import prettymaps
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
-presets = presets().to_dict()
+presets = prettymaps.presets().to_dict()
 
 # Set the title of the app
 st.title("prettymaps")
